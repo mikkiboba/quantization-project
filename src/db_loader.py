3f17601b42673fd928d7baceb6529711f6f4a51d
mikkiDev <mikkimail.games@gmail.com>
2026-08-27 18:31:55 +0200

    fixed and renamed the loader function

--- src/db_loader.py
+++ src/db_loader.py
@@ -1,63 +1,60 @@
-import sys
-from datasets import load_dataset, Dataset
-import os
+from __future__ import annotations
+
+from datasets import Dataset, load_dataset
 
 
-TEXT_REPEAT: int = 50
-
-
-class DatabaseNotFoundError(Exception):
-    """Exception raised for when the database is not found."""
+class DatabaseError(Exception):
+    """Exception raised for when the database cannot be loaded or has an unexpected error."""
     def __init__(self, message: str):
         self.message = message
         super().__init__(self.message)
 
 
-def load(db_name: str, db_version: str | None = None, db_split: str = "test", num_samples: int | None = None, seed: int = 42) -> Dataset:
-    """Return the dataset. Slice if `num_samples` is specified
+def load_dataset_subset(
+        database_name: str,
+        *,
+        database_split: str     = "test",
+        config: str | None      = None,
+        num_samples: int | None = None,
+        seed: int               = 42
+) -> Dataset:
+    """
+    Load a dataset split and select a random subset. (fixed seed)
 
     Args:
-        db_name: Name of the dataset.
-        db_version: If needed, the version of the dataset.
-        db_split: What portion of dataset to take (test, eval, ...)
-        num_samples: How many samples to take from the dataset.
+        database_name: Name of the dataset.
+        database_split: Which database split to take.
+        config: (can be NULL).
+        num_samples: How many samples to take (can be NULL).
         seed: Random seed.
-
-    Returns:
-        The dataset.
     
-    Raises:
-        DatabaseNotFoundError if the dabased cannot be loaded.
+    Returns:
+        Random subset.
     """
-    print("-"*TEXT_REPEAT)
-    print(f"> Loading dataset \"{db_name}\"...")
     try:
-        args = [db_name]
-        if db_version:
-            args.append(db_version)
-        ds: Dataset = load_dataset(*args, split=db_split)
+        if config is not None:
+            dataset = load_dataset(
+                database_name,
+                config,
+                split = database_split
+            )
+        else:
+            dataset = load_dataset(
+                database_name,
+                split = database_split
+            )
+    except Exception as e:
+        raise DatabaseError(
+            f"Could not download dataset '{database_name}' "
+            f"(split = '{database_split}', config = '{config})"
+        ) from e
 
-        if num_samples and num_samples < len(ds):
-            ds = ds.shuffle(seed=seed).select(range(num_samples))
+    if num_samples is not None:
+        if num_samples > len(dataset):
+            raise ValueError(f"Requested {num_samples} samples, but only contains {len(dataset)}")
 
-        print(f"\t✔ Successfully loaded dataset \"{db_name}\".")
-        print(f"\t- Sliced {db_split} instances: {len(ds)}.")
-        print(f"\t- Columns: {ds.column_names}.")
-        print("-"*TEXT_REPEAT+'\n')
-        return ds
-    except Exception:
-        raise DatabaseNotFoundError(f"Failed to load database \"{db_name}\".")
+        dataset = dataset.shuffle(seed=seed).select(range(num_samples))
 
+    return dataset
+        
 
-def save_local_benchmark(num_samples: int = 200):
-    """Prepare and save benchmark datasets locally in /data"""
-    os.makedirs("data", exist_ok=True)
-    print(f"> Preparing local benchmark datasets ({num_samples} samples)")
-
-    cnn_ds = load("abisee/cnn_dailymail", db_version="3.0.0", num_samples=num_samples)
-    cnn_ds.save_to_disk("data/cnn_dailymail_sample")
-
-    xsum_ds = load("EdinburghNLP/xsum", num_samples=num_samples)
-    xsum_ds.save_to_disk("data/xsum_sample")
-
-    print(f"✔ Successfully saved datasets in /data folder.")
